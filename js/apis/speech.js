3de31262080a08f20b7bc591ba7b330c1607f0a5
Ignacy <ignacy130@gmail.com>
2016-04-22 16:50:01 +0200

    cardboard base

---
+++ js/apis/speech.js
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 4/21/2016.
+ */
+var recognition = new webkitSpeechRecognition();
+recognition.interimResults = false;
+recognition.onresult = function(event) {
+    alert((event.results[0])[0].transcript);
+    console.log(event)
+}
+recognition.lang = "en-GB";
+
+recognition.start();
