de1d7de60acc871e19ef3d43d2797514db153df0
Mateusz Chodkowski <chotkos@gmail.com>
2016-04-23 14:14:46 +0200

    Scene objects

--- js/renderers/SceneObjects.js
+++ js/renderers/SceneObjects.js
@@ -1,3 +1,11 @@
 /**
  * Created by admin on 4/23/2016.
  */
+var mesh = null;
+function initMesh(scene) {
+    var loader = new THREE.JSONLoader();
+    loader.load('./models/station_concept_plus90.json', function(geometry) {
+        mesh = new THREE.Mesh(geometry);
+        scene.add(mesh);
+    });
+}
