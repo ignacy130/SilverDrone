b63b56870d8f4cd4555b01e785721dc67acef1cb
Dabler <podbielski.grzegorz@gmail.com>
2016-04-24 09:53:08 +0200

    Added drawing base again

--- js/renderers/SceneObjects.js
+++ js/renderers/SceneObjects.js
@@ -4,41 +4,41 @@
 function getMarsBase(scene) {
     var loader = new THREE.JSONLoader();
 
-    // var loadBases = function () {
-    //     var mesh = null;
-    //     loader.load('./models/station_concept_sizex15.json', function (geometry) {
-    //         mesh = new THREE.Mesh(geometry);
-    //         mesh.rotateX(Math.PI / 2);
-    //         var colorMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.DoubleSide});
-    //         mesh.material = colorMaterial;
-    //         mesh.position.x = 350;
-    //         mesh.position.y = -35;
-    //         mesh.scale.x = 4;
-    //         mesh.scale.y = 4;
-    //         mesh.scale.z = 4;
-    //         scene.add(mesh);
-    //
-    //         var m2 = mesh.clone();
-    //         m2.position.z = 150;
-    //         scene.add(m2);
-    //
-    //         var m3 = mesh.clone();
-    //         m3.position.z = 750;
-    //         m3.position.x = 500;
-    //         m3.rotateX(Math.PI*5/2 );
-    //         m3.scale.x = 2;
-    //         m3.scale.y = 2;
-    //         m3.scale.z = 2;
-    //         //m3.rotate.y = Math.PI/2;
-    //         scene.add(m3);
-    //         var m4 = m3.clone();
-    //         m4.position.x=650;
-    //         scene.add(m4);
-    //         var m5 = m3.clone();
-    //         m5.position.x=800;
-    //         scene.add(m5);
-    //     });
-    // }
+    var loadBases = function () {
+        var mesh = null;
+        loader.load('./models/station_concept_sizex15.json', function (geometry) {
+            mesh = new THREE.Mesh(geometry);
+            mesh.rotateX(Math.PI / 2);
+            var colorMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.DoubleSide});
+            mesh.material = colorMaterial;
+            mesh.position.x = 350;
+            mesh.position.y = -35;
+            mesh.scale.x = 4;
+            mesh.scale.y = 4;
+            mesh.scale.z = 4;
+            scene.add(mesh);
+    
+            var m2 = mesh.clone();
+            m2.position.z = 150;
+            scene.add(m2);
+    
+            var m3 = mesh.clone();
+            m3.position.z = 750;
+            m3.position.x = 500;
+            m3.rotateX(Math.PI*5/2 );
+            m3.scale.x = 2;
+            m3.scale.y = 2;
+            m3.scale.z = 2;
+            //m3.rotate.y = Math.PI/2;
+            scene.add(m3);
+            var m4 = m3.clone();
+            m4.position.x=650;
+            scene.add(m4);
+            var m5 = m3.clone();
+            m5.position.x=800;
+            scene.add(m5);
+        });
+    }
 
     var loadObject = function(src,material,x,y, z, scaleX,scaleY,scaleZ,rotateX,rotateY,rotateZ){
         var mesh = null;
